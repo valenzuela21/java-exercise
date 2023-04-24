1709de6ef8b7db5736bc2bc1cc98032f85100fe4
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-04-24 17:02:54 -0500

    java

--- poo_herencia/src/org/davidvalenzuela/ejemplo/EjemploHerencia.java
+++ poo_herencia/src/org/davidvalenzuela/ejemplo/EjemploHerencia.java
@@ -1,6 +1,7 @@
 package org.davidvalenzuela.ejemplo;
 
 import org.davidvalenzuela.pooherencia.Alumno;
+import org.davidvalenzuela.pooherencia.AlumnoInternacional;
 import org.davidvalenzuela.pooherencia.Profesor;
 
 public class EjemploHerencia {
@@ -8,6 +9,20 @@ public class EjemploHerencia {
         Alumno alumno = new Alumno();
         alumno.setName("Andres");
         alumno.setSecondName("Guzma");
+        alumno.setNotaCastellano(6.6);
+        alumno.setNotaCastellano(5.5);
+        alumno.setNotaMatematica(3.5);
+
+
+        AlumnoInternacional alumnoInternacional =  new AlumnoInternacional();
+        alumnoInternacional.setName("Peter");
+        alumnoInternacional.setSecondName("Gosling");
+        alumnoInternacional.setPais("Australia");
+        alumnoInternacional.setAge(16);
+        alumnoInternacional.setInstitucion("Instituto Nacional");
+        alumnoInternacional.setNotaIdiomas(6.8);
+        alumnoInternacional.setNotaCastellano(5.5);
+        alumnoInternacional.setNotaMatematica(7.0);
 
         Profesor profesor = new Profesor();
         profesor.setName("Luci");
@@ -15,7 +30,14 @@ public class EjemploHerencia {
         profesor.setAsignatura("Matemáticas");
 
         System.out.println(alumno.getName() + " " +  alumno.getSecondName());
-        System.out.println("Profesor" + profesor.getAsignatura() +":"+ profesor.getName() + " " +  profesor.getSecondName()) ;
-
+        System.out.println("Profesor " + profesor.getAsignatura() +" : "+ profesor.getName() + " : " +  profesor.getSecondName()) ;
+        System.out.println(alumnoInternacional.getName() + " " + alumnoInternacional.getSecondName() + " " + alumnoInternacional.getInstitucion() + " " + alumnoInternacional.getPais() );
+        Class clase =  alumnoInternacional.getClass();
+        while(clase.getSuperclass()  !=  null){
+            String hija  = clase.getName();
+            String padre = clase.getSuperclass().getName();
+            System.out.println(hija +  " es una clase hija de la clase padre " +  padre);
+            clase =  clase.getSuperclass();
+        }
     }
 }
